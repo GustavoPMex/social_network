e71e787bce805d745b2f120a80a13835e2a1073f
Gustavo Mex <gustavoppymex@gmail.com>
2021-03-26 23:12:08 -0600

    Configuración previa a la modificación de los templates en friends

--- friends/urls.py
+++ friends/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import FriendsView, ProfileFriend, SearchView, send_friend_request, accept_friend_request
+from .views import FriendsView, ProfileFriend, SearchViewPerson, SearchViewFriends, send_friend_request, accept_friend_request
 
 friends_urls = ([
     path('', FriendsView.as_view(), name='list'),
-    path('search/', SearchView.as_view(), name='search'),
-    path('<str:slug>/', ProfileFriend.as_view(), name='profile'),
+    path('search/', SearchViewPerson.as_view(), name='search'),
+    path('search_list/', SearchViewFriends.as_view(), name='search_friend'),
+    path('profile/<str:slug>/', ProfileFriend.as_view(), name='profile'),
     path('send_friend_request/<int:userID/', send_friend_request, name='Send_request'),
     path('accept_friend_request/<int:requestID>/', accept_friend_request, name='accept_request'),
 ], 'friends')
--- friends/views.py
+++ friends/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, DetailView
 from .models import UserFriends
 from registration.models import Profile
 # Create your views here.  zqaatw
-
+from django.contrib.auth.models import User
  
 class FriendsView(ListView):
     model = UserFriends
@@ -17,23 +17,20 @@ class ProfileFriend(DetailView):
     model = Profile
     template_name = 'friends/friend_profile.html'
 
-class SearchView(ListView):
+class SearchViewPerson(ListView):
     model = Profile
     template_name = 'friends/search_friend_profile.html'
     context_object_name = 'friend'
 
     def get_queryset(self):
-        result = super(SearchView, self).get_queryset()
+        result = super(SearchViewPerson, self).get_queryset()
         query = self.request.GET.get('search')
 
-        print('Hola')
-        print(query)
-
         if query:
-            postresult = Profile.objects.filter(friend_user_code__contains=query)
+            query_result = Profile.objects.filter(friend_user_code__contains=query)
 
-            if postresult:
-                result = postresult
+            if query_result:
+                result = query_result
             else:
                 result = 'no results'
         else:
@@ -42,7 +39,26 @@ class SearchView(ListView):
         return result
     
     
+class SearchViewFriends(ListView):
+    model = Profile
+    template_name = 'friends/search_friend_list.html'
+    context_object_name = 'list_results'
 
+    def get_queryset(self):
+        query = super(SearchViewFriends, self).get_queryset()
+        query = self.request.GET.get('search_friend')
+
+        if query:
+            query_result = Profile.objects.filter(user_name__username__contains=query) 
+
+            if query_result:
+                result = query_result 
+            else:
+                result = 'no results'
+        else:
+            result = 'no results'
+        
+        return result
 
 def send_friend_request(request, userID):
     from_user = request.user 
@@ -60,7 +76,7 @@ def send_friend_request(request, userID):
 
 def accept_friend_request(request, requestID):
     friend_request = FriendRequest.objects.get(id=requestID)
-    if friend_reques.to_user == request.user:
+    if friend_request.to_user == request.user:
         friend_request.to_user.friends.add(friend_request.from_user)
         friend_request.from_user.friends.add(friend_request.to_user)
         friend_request.delete()
